f6172b14c00742aecf8c4a60fa10cf34c108399f
Vasile Gorcinschi <vasigorc@gmail.com>
2023-09-28 16:09:16 -0400

    :truck: Renamed a file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use crate::customtypes::{Point, Rectangle};
+use crate::custom_types::{Point, Rectangle};
 
 mod primitives;
-mod customtypes;
+mod custom_types;
 
 fn main() {
     let person = Rectangle::new(Point::new(1.0, 5.0), Point::new(7.0, 10.0));
