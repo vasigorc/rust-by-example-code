54d11d32759dd6ae8e254d243ddd9bfa2bc868c0
Vasile Gorcinschi <vasigorc@gmail.com>
2023-10-02 12:11:22 -0400

    :necktie: Added flow_of_control example

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use crate::custom_types::{Point, Rectangle};
 
 mod primitives;
 mod custom_types;
+mod flow_of_control;
 
 fn main() {
-    let person = Rectangle::new(Point::new(1.0, 5.0), Point::new(7.0, 10.0));
-    assert_eq!(person.rect_area(), 30.0)
+    flow_of_control::main()
 }
